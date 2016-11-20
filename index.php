c57e0f64d25f83c923b8e50fb8bb30264705bbe8
Lanre Adelowo <adelowomailbox@gmail.com>
2016-11-20 21:26:39 +0100

    Routes for login

--- index.php
+++ index.php
@@ -2,23 +2,136 @@
 
 require_once "vendor/autoload.php";
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Respect\Validation\Validator as v;
+
+define("APP_NAME", "Opensource Swag");
+define("USER_TYPE", "user_type");
+define("LOGGED_IN_USER", "logged");
 
 $app = new \Slim\App();
 
+\App\Session::getInstance()->start();
 
 (new Dotenv\Dotenv(__DIR__))->load();
 
 $container = $app->getContainer();
 
+$container['session'] = function (\Interop\Container\ContainerInterface $container) {
+    return \App\Session::getInstance();
+};
+
+$container['csrf'] = function (\Interop\Container\ContainerInterface $container) {
+    return new \Slim\Csrf\Guard();
+};
+
+$container['auth'] = function (Interop\Container\ContainerInterface $container) {
+    return new \App\Http\Middleware\Authenticate($container);
+};
+
+$container['is_authenticated_user'] = function (\Interop\Container\ContainerInterface $container) {
+    return new \App\Http\Middleware\RedirectIfAuthenticated($container);
+};
+
+$container["db"] = function (\Interop\Container\ContainerInterface $container) {
+
+    return \Doctrine\DBAL\DriverManager::getConnection(
+        [
+            "url" => "sqlite:///data/gbowo.sqlite"
+        ],
+        new \Doctrine\DBAL\Configuration()
+    );
+};
+
+$container['view'] = function (\Interop\Container\ContainerInterface $container) {
+
+    $view = new \Slim\Views\Twig('views'); //no fancy caching or crap here. Take this to production, thou mustn't!!!
+
+    $basePath = rtrim(
+        str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/'
+    );
+
+    $view->addExtension(new \Slim\Views\TwigExtension($container['router'], $basePath));
+
+
+    return $view;
+};
+
+$app->add($container->get("csrf"));
+
+$app->get("/", function (
+    Request $request,
+    Response $response
+) use ($container) {
+
+    return $response->withRedirect(
+        $container->get("router")
+            ->pathFor("dashboard.index")
+    );
+
+});
+
+$app->get("/logout", function (
+    Request $request,
+    Response $response
+) use ($container) {
+
+    /**
+     * @var \App\Session $session
+     */
+    $session = $container->get("session");
+
+    $session->destroy();
+
+    return $response->withRedirect(
+        $container->get("router")->pathFor("app.login")
+    );
+});
+
+$loginValidators = [
+    "mail" => v::notBlank()->email(),
+    "pass" => v::notBlank()
+];
+
+$isAuthenticatedMiddleware = $container->get("is_authenticated_user");
+
+$app->get("/login", 'App\Http\Controller\Account\LoginController:show')
+    ->setName("app.login")
+    ->add($isAuthenticatedMiddleware);
+
+$app->post("/login", 'App\Http\Controller\Account\LoginController:login')
+    ->add($isAuthenticatedMiddleware)
+    ->add(new \DavidePastore\Slim\Validation\Validation($loginValidators));
+
+$registrationValidators = [
+    "fullname" => v::notBlank()->alpha()->length(6, 50),
+    "mail" => v::notBlank()->email(),
+    "pass" => v::notBlank()
+];
+
+$app->get("/signup", 'App\Http\Controller\Account\RegisterController:show')
+    ->setName("app.register")
+    ->add($isAuthenticatedMiddleware);
+
+$app->post("/signup", 'App\Http\Controller\Account\RegisterController:create')
+    ->add($isAuthenticatedMiddleware)
+    ->add(new \DavidePastore\Slim\Validation\Validation($registrationValidators));
 
 
 $app->group("/dashboard", function () use ($app) {
 
-});
+    $app->get("/", 'App\Http\Controller\User\HomeController:home')
+        ->setName("dashboard.index");
+
+})->add($container->get("auth"));
 
 
 $app->group("/admin", function () use ($app) {
 
-});
+})
+    ->add($container->get("auth"))
+    ->add(new \App\Http\Middleware\SuperCow($container));
+
 
 $app->run();
